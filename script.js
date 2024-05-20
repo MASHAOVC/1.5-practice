9523bc6555bb5a0aa1469c41cf2bd62e0f373ff0
Maria <masha-kem133@mail.ru>
2024-05-20 22:47:05 +0200

    gav

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-new Swiper('.main');
+new Swiper('.swiper-container', {
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    slidesPerView: 1.3,
+
+    spaceBetween: 16,
+
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+    },
+
+    mousewheel: {
+        sensitivity: 1,
+    },
+
+    loop: true,
+});
