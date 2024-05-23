0d7d888705129b7b41911663ceae1c923a49390a
Maria <masha-kem133@mail.ru>
2024-05-24 00:14:31 +0200

    gav

--- script.js
+++ script.js
@@ -48,48 +48,46 @@ new Swiper('.swiper-container', {
 let showMoreButton = document.querySelector('.show-more');
 let [icon, text] = showMoreButton.children;
 let hiddenItems768 = document.querySelectorAll('.list__list-item--hidden768');
-let showenItems1120 = document.querySelectorAll('.list__list-item--show1120');
+let hiddenItems1100 = document.querySelectorAll('.list__list-item--hidden1100');
 let lenovoItem = document.querySelector('.hidden-showed');
 let isOpen = false;
 
 showMoreButton.addEventListener('click', function () {
     if (isOpen) {
-        if (window.innerWidth >= 768 && window.innerWidth < 1060) {
+        if (window.innerWidth >= 768 && window.innerWidth < 1100) {
             
             for (let i = 0; i < hiddenItems768.length; i++) {
                 let item = hiddenItems768[i];
                 item.style.display = 'none';
             }
 
-        } else if (window.innerWidth >= 1060) {
+        } else if (window.innerWidth >= 1100) {
             
-            for (let i = 0; i < showenItems1120.length; i++) {
-                let item = showenItems1120[i];
+            for (let i = 0; i < hiddenItems1100.length; i++) {
+                let item = hiddenItems1100[i];
             item.style.display = 'none';
             }
 
-            lenovoItem.style.display = 'none';
         }
 
         icon.src = 'statics/icon.png';
         text.textContent = 'Показать все';
         isOpen = false;     
     } else {
-        if (window.innerWidth >= 768 && window.innerWidth < 1060) {
+        if (window.innerWidth >= 768 && window.innerWidth < 1100) {
 
             for (let i = 0; i < hiddenItems768.length; i++) {
                 let item = hiddenItems768[i];
                 item.style.display = 'flex';
             }
         
-        } else if (window.innerWidth >= 1060) {
+        } else if (window.innerWidth >= 1100) {
 
-            for (let i = 0; i < showenItems1120.length; i++) {
-                let item = showenItems1120[i];
+            for (let i = 0; i < hiddenItems1100.length; i++) {
+                let item = hiddenItems1100[i];
             item.style.display = 'flex';
         }
 
-        lenovoItem.style.display = 'flex';
     }
     
         icon.src = 'statics/icon-close.png';
