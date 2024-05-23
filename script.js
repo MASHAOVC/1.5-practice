51dadf5aefa366c2bef0042d4b29b979e17481cc
Maria <masha-kem133@mail.ru>
2024-05-23 14:39:07 +0200

    gav

--- script.js
+++ script.js
@@ -47,21 +47,54 @@ new Swiper('.swiper-container', {
 
 let showMoreButton = document.querySelector('.show-more');
 let [icon, text] = showMoreButton.children;
+let hiddenItems768 = document.querySelectorAll('.list__list-item--hidden768');
+let showenItems1120 = document.querySelectorAll('.list__list-item--show1120');
+let lenovoItem = document.querySelector('.hidden-showed');
 let isOpen = false;
+console.log(hiddenItems768)
 
-showMoreButton.addEventListener('click' function () {
+showMoreButton.addEventListener('click', function () {
     if (isOpen) {
-
         if (window.innerWidth >= 768 && window.innerWidth < 1060) {
-
+            
+            for (let i = 0; i < hiddenItems768.length; i++) {
+                let item = hiddenItems768[i];
+                item.style.display = 'none';
+            }
 
         } else if (window.innerWidth >= 1060) {
+            
+            for (let i = 0; i < showenItems1120.length; i++) {
+                let item = showenItems1120[i];
+            item.style.display = 'none';
+            }
 
+            lenovoItem.style.display = 'none';
         }
-    
-    icon.src = 'statics\icon-close.png';
-    text.textContent = 'Скрыть'    
-    isOpen = true;
-    }
 
+        icon.src = 'statics\icon.png';
+        text.textContent = 'Показать все';
+        isOpen = false;     
+    } else {
+        if (window.innerWidth >= 768 && window.innerWidth < 1060) {
+
+            for (let i = 0; i < hiddenItems768.length; i++) {
+                let item = hiddenItems768[i];
+                item.style.display = 'flex';
+            }
+        
+        } else if (window.innerWidth >= 1060) {
+
+            for (let i = 0; i < showenItems1120.length; i++) {
+                let item = showenItems1120[i];
+            item.style.display = 'flex';
+        }
+
+        lenovoItem.style.display = 'flex';
+    }
+    
+        icon.src = 'statics\icon-close.png';
+        text.textContent = 'Скрыть';    
+        isOpen = true;
+    }
 });
