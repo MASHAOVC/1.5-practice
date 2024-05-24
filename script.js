2000c6fecd7346fcbbc1a2d7abff31950c61d36e
Maria <masha-kem133@mail.ru>
2024-05-24 18:48:53 +0200

    gav

--- script.js
+++ script.js
@@ -49,48 +49,59 @@ let showMoreButton = document.querySelector('.show-more');
 let [icon, text] = showMoreButton.children;
 let hiddenItems768 = document.querySelectorAll('.list__list-item--hidden768');
 let hiddenItems1100 = document.querySelectorAll('.list__list-item--hidden1100');
+
 let isOpen = false;
 
-showMoreButton.addEventListener('click', function () {
+function normalize() {
+    if (window.innerWidth < 768) {
+        showAll();
+        return;
+    }
+
     if (isOpen) {
-        if (window.innerWidth >= 768 && window.innerWidth < 1100) {
-            
-            for (let i = 0; i < hiddenItems768.length; i++) {
-                let item = hiddenItems768[i];
-                item.style.display = 'none';
-            }
-
-        } else if (window.innerWidth >= 1100) {
-            
-            for (let i = 0; i < hiddenItems1100.length; i++) {
-                let item = hiddenItems1100[i];
-                item.style.display = 'none';
-            }
-
-        }
-
-        icon.src = 'statics/icon.png';
-        text.textContent = 'Показать все';
-        isOpen = false;     
+        showAll();
     } else {
         if (window.innerWidth >= 768 && window.innerWidth < 1100) {
 
             for (let i = 0; i < hiddenItems768.length; i++) {
                 let item = hiddenItems768[i];
-                item.style.display = 'flex';
+                item.style.display = 'none';
             }
         
         } else if (window.innerWidth >= 1100) {
 
+            for (let i = 0; i < hiddenItems768.length; i++) {
+                let item = hiddenItems768[i];
+                item.style.display = 'flex';
+            }
             for (let i = 0; i < hiddenItems1100.length; i++) {
                 let item = hiddenItems1100[i];
-                item.style.display = 'flex';
-        }
+                item.style.display = 'none';
+            }
 
+        }
     }
-    
+}
+
+function showAll() {
+    for (let i = 0; i < hiddenItems768.length; i++) {
+        let item = hiddenItems768[i];
+        item.style.display = 'flex';
+    }
+}
+
+showMoreButton.addEventListener('click', function() {
+    isOpen = !isOpen;
+
+    if (isOpen) {
         icon.src = 'statics/icon-close.png';
         text.textContent = 'Скрыть';    
-        isOpen = true;
+    } else {
+        icon.src = 'statics/icon.png';
+        text.textContent = 'Показать все';
     }
+
+    normalize();
 });
+
+window.addEventListener('resize', normalize);
